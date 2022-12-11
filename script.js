8015c3a5a4284a798ba87487e5012a2928c734e4
Lukasz Konrad <lukasz.konrad@gmail.com>
2022-12-11 15:08:27 +0100

    function init add

--- script.js
+++ script.js
@@ -1,39 +1,47 @@
 {
-const formElement = document.querySelector(".form");
-const amount = document.querySelector(".form__amount");
-const Optional = document.querySelector(".form__currency");
-const Result = document.querySelector(".result");
-
-formElement.addEventListener("submit", (event) => {
-    event.preventDefault();
-
-  const value = +amount.value;
-  const currency = Optional.value;
   
-  const calculateResult = (value, currency) => {
-  const rateEUR = 4.68;
-  const rateGBP = 5.44;
-  const rateUSD = 4.51;
+    const calculateResult = (value, currency) => {
 
-    switch (currency) {
-      case "EUR" :
-        return value / rateEUR;
-      
-      case "GBP" :
-        return value / rateGBP;
-      
-      case "USD" :
-        return value / rateUSD;
-  
-    }
-  };
-  
-  const result = calculateResult(value, currency);
+      const rateEUR = 4.68;
+      const rateGBP = 5.44;
+      const rateUSD = 4.51;
 
-  
-  Result.innerText = `${result.toFixed(2)} ${currency}`;
-  
+      switch (currency) {
+        case "EUR":
+          return value / rateEUR;
 
-})
+        case "GBP":
+          return value / rateGBP;
+
+        case "USD":
+          return value / rateUSD;
+
+      }
+    };
+  
+  
+  const init = () => {
+
+  const formElement = document.querySelector(".form");  
+  formElement.addEventListener("submit", (event) => {
+      event.preventDefault();
+ 
+  const amount = document.querySelector(".form__amount");
+  const Optional = document.querySelector(".form__currency");
+  const Result = document.querySelector(".result");
+    
+    const value = +amount.value;
+    const currency = Optional.value;
+    
+    const result = calculateResult(value, currency);
+
+
+    Result.innerText = `${result.toFixed(2)} ${currency}`;
+
+
+  })
+};
+
+init();
 
 }
